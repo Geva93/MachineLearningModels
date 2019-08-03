66d9cc8e17d41d48dc982af99e9343295074f260
Geva <geva93@gmail.com>
2019-08-02 22:07:28 -0400

    first adjustment for QUANDLE

--- regression.py
+++ regression.py
@@ -1,7 +1,12 @@
-import pandas as od
+import pandas as pd
 import quandl as Quandl
 
 api_k = "VKApkPmDCQfgmTvuE_xy"
 df = Quandl.get('WIKI/GOOGL', api_key = api_k)
 
+df = df[['Adj. Open','Adj. High','Adj. Low','Adj. Close','Adj. Volume']]
+df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close']) / df['Adj. Close'] * 100.0
+df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0
+
+df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
 print(df.head())

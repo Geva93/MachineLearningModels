77c344469ca08f69fe88a7927658d47299c84cf4
Geva <geva93@gmail.com>
2019-08-03 15:54:04 -0400

    working with pickle - saving a classifier in a file

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Building Machine learning models from scratch
 Step 1:
 train LinearRegression and svm.SVR model on stock price
+#Build a classifier
+#Pickle
--- regression.py
+++ regression.py
@@ -3,11 +3,16 @@ from os.path import join, dirname
 from dotenv import load_dotenv
 import pandas as pd
 import quandl as Quandl
-import math
+import math, datetime
 import numpy as np
 from sklearn import preprocessing, svm
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+import pickle
+from matplotlib import style
+
+style.use('ggplot')
 
 
 dotenv_path = join(dirname(__file__), '.env')
@@ -27,14 +32,43 @@ df.fillna(-99999, inplace = True) #Fill Non available fields, in ML don't work w
 
 forecast_out = int(math.ceil(0.01*len(df))) # round up to the nearst whole
 df['label'] = df[forecast_col].shift(-forecast_out)
-df.dropna(inplace=True)
 
 x = np.array(df.drop(['label'], 1))
-y = np.array(df['label'])
+
 x = preprocessing.scale(x) #normalize the data before fit
+x = x[:-forecast_out]
+x_lately = x[-forecast_out:]
+
+df.dropna(inplace=True)
+y = np.array(df['label'])
+
 
 x_train, x_test, y_train, y_test = train_test_split(x,y,test_size = 0.2)
-clf = LinearRegression(n_jobs = 10)
+clf = LinearRegression(n_jobs = 1)
 clf.fit(x_train, y_train)
+with open('linearregression.pickle', 'wb') as f:
+    pickle.dump(clf, f)
+pickle_in = open('linearregression.pickle','rb')
+clf = pickle.load(pickle_in)
 accuracy = clf.score(x_test, y_test)
-print(accuracy)
+#print(accuracy)
+forecast_set = clf.predict(x_lately)
+#print(forecast_set, accuracy, forecast_out)
+
+df['Forecast'] = np.nan
+
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
+
+for i in forecast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += one_day
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)] + [i]
+df['Adj. Close'].plot()
+df['Forecast'].plot()
+plt.legend(loc = 4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+#plt.show()
